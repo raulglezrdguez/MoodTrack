9b3edbb86fee1a96508cc113dcbc566a8866c545
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-11-17 16:18:54 -0500

    AsyncStorage added

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,14 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
+
+const storageKey = 'moods-data';
+
+type AppData = {
+  moods: MoodOptionWithTimestamp[];
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -21,10 +30,39 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
   const handleSelectedMood = useCallback((mood: MoodOptionType) => {
-    setMoodList(currentList => [
-      ...currentList,
-      { mood, timestamp: Date.now() },
-    ]);
+    setMoodList(currentList => {
+      const newList = [...currentList, { mood, timestamp: Date.now() }];
+      setData({ moods: newList });
+      return newList;
+    });
+  }, []);
+
+  const getData = async (): Promise<AppData | null> => {
+    try {
+      const data = await AsyncStorage.getItem(storageKey);
+      if (data) {
+        return JSON.parse(data);
+      }
+      return null;
+    } catch {
+      return null;
+    }
+  };
+
+  const setData = async (data: AppData) => {
+    try {
+      await AsyncStorage.setItem(storageKey, JSON.stringify(data));
+    } catch {}
+  };
+
+  useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getData();
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
   }, []);
 
   return (
