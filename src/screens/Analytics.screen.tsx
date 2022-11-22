2e2a9cbdebf899dbc5922fb9e61f0c683dcd1487
raulglezrdguez <raulglezrguez@gmail.com>
2022-11-22 12:12:23 -0500

    PieChart from victory added

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { memo, useCallback } from 'react';
+import { StyleSheet, View, useWindowDimensions } from 'react-native';
+import { VictoryLabel, VictoryPie, VictoryTheme } from 'victory-native';
+import { useAppContext } from '../context/App.provider';
+import { theme } from '../theme';
 
 export const Analytics: React.FC = () => {
+  const { moodList } = useAppContext();
+
+  const moodListData = useCallback(() => {
+    const result: { label: string; y: number }[] = [];
+    moodList.map(ml => {
+      const found = result.find(r => r.label === ml.mood.emoji);
+      if (found) {
+        found.y += 1;
+      } else {
+        result.push({ label: ml.mood.emoji, y: 1 });
+      }
+    });
+    return result;
+  }, [moodList]);
+
+  const { width } = useWindowDimensions();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Analitics</Text>
+      <VictoryPie
+        data={moodListData()}
+        width={width - width / 5}
+        // theme={VictoryTheme.material}
+        // cornerRadius={({ datum }) => datum.y / 2}
+        // innerRadius={({ datum }) => datum.y / 2}
+        // labelComponent={<VictoryLabel angle={-15} />}
+        labels={({ datum }) => datum.label}
+        labelRadius={width / 5}
+        radius={width / 3}
+        innerRadius={width / 8}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+      />
     </View>
   );
 };
